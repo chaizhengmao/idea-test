f008c98a6da515f108f9ee4cc7369bd0d60815f6
zhengmao_chai <chai.zm@asiainfo-sec.com>
2021-08-03 23:54:16 +0800

    test

--- src/com/yexu/ArrayDemo.java
+++ src/com/yexu/ArrayDemo.java
@@ -13,22 +13,24 @@ public class ArrayDemo {
     public static void main(String[] args) {
         new Person().say();
         Object[] array = new String[10];
-//        array[0] = 10;
+        array[0] = 10;
 
         ArrayList<String> list = new ArrayList<>();
         ArrayList<Integer> list1 = new ArrayList<>();
         list.add("zhangsan");
     }
 
-    public static class demo{
+    public static class demo {
 
     }
-    public static class demo1{
+
+    public static class demo1 {
 
     }
 }
-class Person{
-    public void say(){
+
+class Person {
+    public void say() {
         System.out.println(1111);
     }
 }
